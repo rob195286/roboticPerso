8cbe0e3adecd6dbda55ad5d981bd4fba1f2523f6
rob195286 <60742089+rob195286@users.noreply.github.com>
2021-02-14 18:34:33 +0100

    Update arduino_connexion.ino

--- communication/arduino_connexion/arduino_connexion.ino
+++ communication/arduino_connexion/arduino_connexion.ino
@@ -4,40 +4,27 @@
 void setup(){
  
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
 }
 
 void loop(){
-  int message = get_serial_command_from_pi();
+  char message = get_serial_command_from_pi();
   
-  //if (message == 97)
-  if(message >= 1)
+  if (message == 97)// 97 = 'a' dans le message envoyé en dascii 
+  //if(message >= 1) // permet de tester et de voir uniquement les caractère envoyé
   {
     Serial.println(message);    
   }
-  if (message == 98)
+  if (message == 98) // 98 = 'b' dans le message envoyé en dascii 
   {
-    Serial.println(message, DEC);    
+    Serial.println(message);    
   }
-  if (message == 99)
-  {
-    Serial.println(message, DEC);    
-  }  
-}
-
-void blink_led(int time){
-  digitalWrite(13, HIGH);
-   delay(time);
-  digitalWrite(13, LOW);
-  delay(time);
 }
 
 
 int get_serial_command_from_pi(){
-  int byteMessage = 0;
+  char byteMessage = 0;
   if(Serial.available()>0){
     byteMessage = Serial.read();
     }
   return byteMessage;
 }
-
