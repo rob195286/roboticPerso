fea610cd6c47ffa42b524bcf5353edaecffa03c9
rob195286 <60742089+rob195286@users.noreply.github.com>
2021-02-22 22:29:04 +0100

    Update main.py

--- communication/main.py
+++ communication/main.py
@@ -1,18 +1,23 @@
-from communication import ArduinoConnexion 
 from time import sleep
+from serial import *
 
+def sendData(data, periph = "/dev/ttyACM0", baudrate = 9600):
+    # Encode en binaire chaque éléments de data séparément pour l'envoyer
+    # à l'arduino car impossible de faire cette opération sur toute une liste en même temps.
+    ser = Serial(periph, baudrate)        
+    ser.write(data.encode('ascii'))# https://roboticsbackend.com/raspberry-pi-       arduino-serial-communication/
 
+    
 avant = 'a'
 tourner = 'b'
 
-ardConn = ArduinoConnexion()
 x=0
 
 while(x<3):
     sleep(2)
     print(avant)
-    ardConn.sendData(avant)
+    sendData(avant)
     sleep(2)
-    ardConn.sendData(tourner)
+    sendData(tourner)
     print(tourner)
     x += 1
